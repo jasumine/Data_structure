105527f43f39e437a1a5d692a873831170de58a8
지원 <92582758+jasumine@users.noreply.github.com>
2022-12-30 18:46:21 +0900

    문자열 예제
    문자열을 입력했을 때, 문자열 앞과 뒤에  있는 공백을 지우고,  문자와 문자 사이에 공백이 2개일 경우 1개로 출력한다.
    중복되는 공백을 지운 문자열과 공백을 포함하여 글자 수 출력

---
+++ class3/class3/main.cpp
@@ -0,0 +1,64 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// text[0]은 문자로 시작해야한다.
+// 공백이면 없애준다
+// 직전문자가 공백이 아니면 넘어간다.
+int main()
+{
+	string text;
+	while (1)
+	{
+		cout << "$";
+		getline(cin, text);
+		
+		int size = text.size();
+
+		// text[0]가 0이 아닐때까지 반복
+		while (text[0] == ' ')
+		{
+			text.erase(0, 0);
+			size--;
+			for (int i = 0; i < text.size(); i++)
+			{
+				text[i] = text[i + 1];
+			}
+		}
+
+		for (int i = 0; i < size; i++)
+		{
+			// 문자이거나, 공백인데 직전이 문자인 경우 ==continue 나머지 erase
+			if (text[i] != ' ' || (text[i] == ' ' && text[i - 1] != ' '))
+			{
+				continue;
+			}
+			else
+			{
+				// 공백이 아닐때까지 반복
+				while (text[i] ==' ')
+				{
+					text.erase(i, 0); 
+					for (int j = i; j < size; j++)
+					{
+						text[j] = text[j + 1];
+					}
+					size--;
+				}
+			}
+
+		}
+
+		// 문자 끝이 공백이라면 비워준다.
+		if (text[size-1] == ' ')
+		{
+			text[size - 1] = NULL;
+			size--;
+		}
+
+		cout << text << ":" << size << endl;
+	}
+	return 0;
+}
